2fbe756c2bfbe13484f56e65af8298d5fdee457a
Sergey Filkin <sergey.filkin@gmail.com>
2017-04-03 13:21:35 +0300

    modified `media`

--- src/modules/core/style-utils.js
+++ src/modules/core/style-utils.js
@@ -9,7 +9,7 @@ const sizes = {
 export const media = Object.keys(sizes).reduce((accumulator, label) => {
   const emSize = sizes[label] / 16
   accumulator[label] = (...args) => css`
-    @media (max-width: ${emSize}em) {
+    @media (min-width: ${emSize}em) {
       ${css(...args)}
     }
   `
