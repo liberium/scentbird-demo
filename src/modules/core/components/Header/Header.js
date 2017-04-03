a385d99138d7ad4757268e8d7d18d3d493cada4b
Sergey Filkin <sergey.filkin@gmail.com>
2017-04-03 13:22:44 +0300

    modified `Header`

--- src/modules/core/components/Header/Header.js
+++ src/modules/core/components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Header = () => (
-  <header>
-    
-  </header>
+import { media } from 'core/style-utils'
+
+import logo from './logo.svg'
+
+const Header = styled.header`
+
+`
+
+const Logo = styled.img`
+  display: block;
+  ${media.phone`margin: 0 auto;`}
+  ${media.tablet`margin: 0;`}
+  ${media.desktop`margin: 0;`}
+`
+
+export default () => (
+  <Header>
+    <Logo src={logo}/>
+  </Header>
 )
-
-export default Header
